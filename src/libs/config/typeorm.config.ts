8f80caba211efc4c36bff72597a35f79f83b06fa
NarciSource <jwch11@gmail.com>
2025-05-18 14:14:23 +0900

    chore: 도커컴포즈 및 환경변수 업데이트

--- src/libs/config/typeorm.config.ts
+++ src/libs/config/typeorm.config.ts
@@ -4,15 +4,15 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export default {
   useFactory: (): TypeOrmModuleOptions => {
-    const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
+    const { PG_HOST, PG_PORT, PG_USERNAME, PG_PASSWORD, PG_DATABASE } = process.env;
 
     return {
       type: "postgres",
-      host: DB_HOST,
-      port: Number(DB_PORT),
-      username: DB_USERNAME,
-      password: DB_PASSWORD,
-      database: DB_DATABASE,
+      host: PG_HOST,
+      port: Number(PG_PORT),
+      username: PG_USERNAME,
+      password: PG_PASSWORD,
+      database: PG_DATABASE,
       entities: [
         join(process.cwd(), "dist", "**", "*.entity.js"),
         join(process.cwd(), "dist", "**", "*.view.js"),
