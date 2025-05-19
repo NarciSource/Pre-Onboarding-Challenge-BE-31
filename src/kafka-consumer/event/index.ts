0eb0e55fe6c8333470aa5ab9866104da38d9b7ae
NarciSource <jwch11@gmail.com>
2025-05-20 08:39:58 +0900

    feat: Brand/Seller 토픽 반영

--- src/kafka-consumer/event/index.ts
+++ src/kafka-consumer/event/index.ts
@@ -2,3 +2,5 @@ export { default as ProductDeleteEvent } from "./ProductDelete.event";
 export { default as ProductDeleteHandler } from "./ProductDelete.handler";
 export { default as ProductUpsertEvent } from "./ProductUpsert.event";
 export { default as ProductUpsertHandler } from "./ProductUpsert.handler";
+export { default as MerchantUpsertEvent } from "./MerchantUpsert.event";
+export { default as MerchantUpsertHandler } from "./MerchantUpsert.handler";
