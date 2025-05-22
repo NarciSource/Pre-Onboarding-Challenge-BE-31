05f3e993c990f95f83dc03f4482e0736bd27e47d
NarciSource <jwch11@gmail.com>
2025-05-22 16:30:21 +0900

    delete: ViewTable 삭제

--- src/browsing/application/query/Find.handler.ts
+++ src/browsing/application/query/Find.handler.ts
@@ -1,9 +1,8 @@
 import { Inject } from "@nestjs/common";
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 
-import { IQueryRepository, IViewRepository } from "@shared/repositories";
-import { ProductSummaryModel } from "@browsing/infrastructure/mongo/models";
-import { CategoryCatalogView } from "@browsing/infrastructure/rdb/views";
+import { IQueryRepository } from "@shared/repositories";
+import { CategoryCatalogModel, ProductSummaryModel } from "@browsing/infrastructure/mongo/models";
 import FindQuery from "./Find.query";
 
 @QueryHandler(FindQuery)
@@ -11,8 +10,8 @@ export default class FindHandler implements IQueryHandler<FindQuery> {
   constructor(
     @Inject("IProductSummaryQueryRepository")
     private readonly product_summary_query_repository: IQueryRepository<ProductSummaryModel>,
-    @Inject("ICategoryCatalogViewRepository")
-    private readonly category_catalog_repository: IViewRepository<CategoryCatalogView>,
+    @Inject("ICategoryCatalogQueryRepository")
+    private readonly category_catalog_repository: IQueryRepository<CategoryCatalogModel>,
   ) {}
 
   async execute() {
--- src/browsing/infrastructure/rdb/views/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CategoryCatalogView } from "./CategoryCatalog.view";
-export { default as ProductCatalogView } from "./ProductCatalog.view";
-export { default as ProductSummaryView } from "./ProductSummary.view";
