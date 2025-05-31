928369e73a2e04206c35482594117b1b4d3517c0
NarciSource <jwch11@gmail.com>
2025-05-31 15:15:11 +0900

    feat: mongodb 인증제거
    - replica set 구성

--- libs/config/src/mongo.config.ts
+++ libs/config/src/mongo.config.ts
@@ -6,13 +6,10 @@ export default {
   useFactory: (config: ConfigService) => {
     const host = config.get<string>("MONGO_HOST");
     const port = config.get<string>("MONGO_PORT");
-    const user = config.get<string>("MONGO_USER");
-    const password = config.get<string>("MONGO_PASSWORD");
     const db = config.get<string>("MONGO_DATABASE");
 
     return {
-      uri: `mongodb://${user}:${password}@${host}:${port}/${db}`,
-      authSource: "admin",
+      uri: `mongodb://${host}:${port}/${db}`,
     };
   },
 };
