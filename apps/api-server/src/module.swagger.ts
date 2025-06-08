8ba96eae622099e3db25946808ef3bd3fa0a2a3f
NarciSource <jwch11@gmail.com>
2025-06-08 11:36:25 +0900

    fix: Swagger 문서 생성 수정

---
+++ apps/api-server/src/module.swagger.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { CqrsModule } from "@nestjs/cqrs";
+
+import * as product_controllers from "product/presentation/controllers";
+import * as category_controllers from "category/presentation/controllers";
+import * as review_controllers from "review/presentation/controllers";
+import * as browsing_controllers from "browsing/presentation/controllers";
+
+@Module({
+  imports: [CqrsModule],
+  controllers: [
+    ...Object.values(product_controllers),
+    ...Object.values(category_controllers),
+    ...Object.values(review_controllers),
+    ...Object.values(browsing_controllers),
+  ],
+})
+export default class SwaggerAppModule {}
